feca4c56163ade6307b57d6b98d8e168da8cbec6
gallynaut <Connerngallagher@gmail.com>
2021-10-07 16:13:21 -0600

    succesfully reading in json keypair

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,20 @@ import {
   PublicKey,
 } from "@solana/web3.js";
 import fs from "fs";
-import resolve from "dir-resolve";
-import yargs from "yargs";
+// import { readFile } from "fs/promises";
+import resolve from "resolve-dir";
+import yargs from "yargs/yargs";
 import feeds from "./feeds.json";
 
-const argv = yargs(process.argv).options({
-  payerKeypairFile: {
-    type: "string",
-    describe: "Path to keypair file that will pay for transactions.",
-    demand: true,
-    default: "./keypair.json",
-  },
-}).argv;
+const argv = yargs(process.argv.slice(2))
+  .options({
+    payerKeypairFile: {
+      type: "string",
+      describe: "Path to keypair file that will pay for transactions.",
+      demand: true,
+    },
+  })
+  .parseSync();
 
 function toCluster(cluster: string): Cluster {
   switch (cluster) {
@@ -31,14 +33,20 @@ function toCluster(cluster: string): Cluster {
 }
 
 async function main(): Promise<void> {
-  console.log("ARGS", argv);
+  // Setup Solana connection
   const cluster = "devnet";
   const url = clusterApiUrl(toCluster(cluster), true);
   const connection = new Connection(url, "processed");
-  // const payerKeypair = JSON.parse(
-  //   fs.readFileSync(resolve(argv.payerKeypairFile), "utf-8")
-  // );
-  // const payerAccount = new Account(payerKeypair);
+
+  // Read in keypair file
+  // const keypair = resolve(argv.payerKeypairFile);
+  // console.log(keypair);
+
+  const payerKeypair = JSON.parse(
+    fs.readFileSync(resolve(argv.payerKeypairFile), "utf-8")
+  );
+  const payerAccount = new Account(payerKeypair);
+  console.log(payerAccount.publicKey.toString());
 }
 
 main().then(
