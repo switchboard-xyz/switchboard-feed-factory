b2d0be1812173844ac09409da14917d0c599629b
gallynaut <Connerngallagher@gmail.com>
2021-10-07 15:36:18 -0600

    typescript output

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import * as readline from "readline-sync";
+import {
+  Account,
+  Cluster,
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+} from "@solana/web3.js";
+import fs from "fs";
+import resolve from "dir-resolve";
 import yargs from "yargs";
-import feeds from "../feeds.json";
-
-// const argv = await yargs(process.argv).options({
-//   payerKeypairFile: {
-//     type: "string",
-//     describe: "Path to keypair file that will pay for transactions.",
-//     demand: false,
-//     default: undefined,
-//   },
-// }).argv;
+import feeds from "./feeds.json";
 
 const argv = yargs(process.argv).options({
   payerKeypairFile: {
@@ -20,18 +19,35 @@ const argv = yargs(process.argv).options({
   },
 }).argv;
 
-async function main() {
-  // Try to produce a valid banker account.
-  // const banker =
-  //   argv.payerKeypairFile ||
-  //   readline.question("Please enter a path to a payer keypair file: ");
+function toCluster(cluster: string): Cluster {
+  switch (cluster) {
+    case "devnet":
+    case "testnet":
+    case "mainnet-beta": {
+      return cluster;
+    }
+  }
+  throw new Error("Invalid cluster provided.");
+}
+
+async function main(): Promise<void> {
+  console.log("ARGS", argv);
+  const cluster = "devnet";
+  const url = clusterApiUrl(toCluster(cluster), true);
+  const connection = new Connection(url, "processed");
+  // const payerKeypair = JSON.parse(
+  //   fs.readFileSync(resolve(argv.payerKeypairFile), "utf-8")
+  // );
+  // const payerAccount = new Account(payerKeypair);
 }
 
 main().then(
-  () => process.exit(),
+  () => {
+    console.log("Switchboard-EPL-Feeds ran successfully.");
+    process.exit();
+  },
   (err) => {
-    console.error("Failed to complete successfully.");
-    console.error(err);
+    console.log(err);
     process.exit(-1);
   }
 );
