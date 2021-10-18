470a264f659a1c7457dec95026c00c47b5b0080a
Conner Gallagher <ConnerNGallagher@gmail.com>
2021-10-17 23:26:38 -0600

    update script

--- src/utils/updateDataFeed.ts
+++ src/utils/updateDataFeed.ts
@@ -1,11 +1,27 @@
-import { Account, clusterApiUrl, Connection } from "@solana/web3.js";
+import {
+  Account,
+  clusterApiUrl,
+  Connection,
+  Context,
+  PublicKey,
+  SignatureResult,
+} from "@solana/web3.js";
 import yargs from "yargs/yargs";
 import fs from "fs";
 import resolve from "resolve-dir";
 import prompts, { Choice } from "prompts";
 import path from "path";
+import { FactoryOutputJSON } from "../types";
+import {
+  AggregatorState,
+  parseAggregatorAccountData,
+  updateFeed,
+} from "@switchboard-xyz/switchboard-api";
+import { EventEmitter } from "events";
+import { waitFor } from "wait-for-event";
+import { sleep } from "./sleep";
+import chalk from "chalk";
 
-// NOT IMPLEMENTED
 async function main(): Promise<string> {
   const argv = yargs(process.argv.slice(2))
     .options({
@@ -58,45 +74,20 @@ async function main(): Promise<string> {
     },
   ]);
   console.log(`selected ${pickJson.jsonFile}`);
+  const createdFeeds: FactoryOutputJSON[] = JSON.parse(
+    fs.readFileSync(pickJson.jsonFile).toString()
+  );
+  for await (const feed of createdFeeds) {
+    console.log(chalk.blue(`Updating ${feed.name}`));
+    await updateDataFeed(
+      connection,
+      payerAccount,
+      feed.dataFeed,
+      feed.updateAuth
+    );
+    await sleep(1_000);
+  }
 
-  //   let dataFeedPubkey = new PublicKey(argv.dataFeedPubkey);
-  //   let updateAuthPubkey = new PublicKey(argv.updateAuthPubkey);
-  //   let signature = await updateFeed(
-  //     connection,
-  //     payerAccount,
-  //     dataFeedPubkey,
-  //     updateAuthPubkey
-  //   );
-  //   console.log("Awaiting update transaction finalization...");
-  //   let emitter = new EventEmitter();
-  //   let callback = async function (
-  //     signatureResult: SignatureResult,
-  //     ctx: Context
-  //   ) {
-  //     let attempts = 30;
-  //     while (attempts--) {
-  //       let state: AggregatorState = await parseAggregatorAccountData(
-  //         connection,
-  //         dataFeedPubkey
-  //       );
-  //       if (
-  //         state.currentRoundResult.numSuccess +
-  //           state.currentRoundResult.numError !==
-  //         0
-  //       ) {
-  //         console.log(
-  //           `(${dataFeedPubkey.toBase58()}) state.\n`,
-  //           JSON.stringify(state.toJSON(), null, 2)
-  //         );
-  //         break;
-  //       }
-  //       // It may take a few more seconds for the oracle response to be processed.
-  //       await sleep(1_000);
-  //     }
-  //     emitter.emit("Done");
-  //   };
-  //   connection.onSignature(signature, callback, "processed");
-  //   await waitFor("Done", emitter);
   return "";
 }
 
@@ -109,3 +100,66 @@ main().then(
     return "";
   }
 );
+
+async function updateDataFeed(
+  connection: Connection,
+  payerAccount: Account,
+  dataFeed: string,
+  updateAuth: string
+) {
+  const dataFeedPubkey = new PublicKey(dataFeed);
+  const updateAuthPubkey = new PublicKey(updateAuth);
+  const signature = await updateFeed(
+    connection,
+    payerAccount,
+    dataFeedPubkey,
+    updateAuthPubkey
+  );
+  console.log(
+    `Awaiting update transaction finalization... https://explorer.solana.com/tx/${signature}?cluster=devnet`
+  );
+  const emitter = new EventEmitter();
+  const callback = async function (
+    signatureResult: SignatureResult,
+    ctx: Context
+  ) {
+    if (signatureResult.err) {
+      console.error("SignatureError:", signatureResult);
+      return;
+    }
+    let attempts = 30;
+    while (attempts--) {
+      const state: AggregatorState = await parseAggregatorAccountData(
+        connection,
+        dataFeedPubkey
+      );
+      const numSuccess = state.currentRoundResult?.numSuccess
+        ? state.currentRoundResult?.numSuccess
+        : 0;
+      const numError = state.currentRoundResult?.numError
+        ? state.currentRoundResult?.numError
+        : 0;
+
+      if (numSuccess + numError !== 0) {
+        console.log(
+          `(${dataFeedPubkey.toBase58()}) state.\n`,
+          JSON.stringify(state.toJSON(), null, 2)
+        );
+        emitter.emit("Success");
+        break;
+      }
+      // It may take a few more seconds for the oracle response to be processed.
+      await sleep(1_000);
+    }
+    emitter.emit("Done");
+  };
+  connection.onSignature(signature, callback, "finalized");
+  emitter.on("Done", () => {
+    console.error(`${chalk.red("Failed to update data feed")}`);
+  });
+  emitter.on("Success", () => {
+    console.error("Successfully updated data feed");
+    emitter.emit("Done");
+  });
+  await waitFor("Done", emitter);
+}
